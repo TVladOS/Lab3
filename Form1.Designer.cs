d6a0aa4527b5a2573da3a0ca7f8fb9a10a2fed6a
Vladislav Taranenko <61828759+TVladOS@users.noreply.github.com>
2020-04-23 17:02:23 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace ThirdLabNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numerator1 = new System.Windows.Forms.TextBox();
+            this.denominator1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.denominator2 = new System.Windows.Forms.TextBox();
+            this.numerator2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.denominator4 = new System.Windows.Forms.TextBox();
+            this.numerator4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.denominator3 = new System.Windows.Forms.TextBox();
+            this.numerator3 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.r_numerator = new System.Windows.Forms.Label();
+            this.r_denominator = new System.Windows.Forms.Label();
+            this._r_denominator = new System.Windows.Forms.Label();
+            this._r_numerator = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.solute = new System.Windows.Forms.Button();
+            this.clea_r = new System.Windows.Forms.Button();
+            this.label22 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // numerator1
+            // 
+            resources.ApplyResources(this.numerator1, "numerator1");
+            this.numerator1.Name = "numerator1";
+            // 
+            // denominator1
+            // 
+            resources.ApplyResources(this.denominator1, "denominator1");
+            this.denominator1.Name = "denominator1";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // denominator2
+            // 
+            resources.ApplyResources(this.denominator2, "denominator2");
+            this.denominator2.Name = "denominator2";
+            // 
+            // numerator2
+            // 
+            resources.ApplyResources(this.numerator2, "numerator2");
+            this.numerator2.Name = "numerator2";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // denominator4
+            // 
+            resources.ApplyResources(this.denominator4, "denominator4");
+            this.denominator4.Name = "denominator4";
+            // 
+            // numerator4
+            // 
+            resources.ApplyResources(this.numerator4, "numerator4");
+            this.numerator4.Name = "numerator4";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // denominator3
+            // 
+            resources.ApplyResources(this.denominator3, "denominator3");
+            this.denominator3.Name = "denominator3";
+            // 
+            // numerator3
+            // 
+            resources.ApplyResources(this.numerator3, "numerator3");
+            this.numerator3.Name = "numerator3";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // r_numerator
+            // 
+            resources.ApplyResources(this.r_numerator, "r_numerator");
+            this.r_numerator.Name = "r_numerator";
+            // 
+            // r_denominator
+            // 
+            resources.ApplyResources(this.r_denominator, "r_denominator");
+            this.r_denominator.Name = "r_denominator";
+            // 
+            // _r_denominator
+            // 
+            resources.ApplyResources(this._r_denominator, "_r_denominator");
+            this._r_denominator.Name = "_r_denominator";
+            // 
+            // _r_numerator
+            // 
+            resources.ApplyResources(this._r_numerator, "_r_numerator");
+            this._r_numerator.Name = "_r_numerator";
+            // 
+            // label15
+            // 
+            resources.ApplyResources(this.label15, "label15");
+            this.label15.Name = "label15";
+            // 
+            // label14
+            // 
+            resources.ApplyResources(this.label14, "label14");
+            this.label14.Name = "label14";
+            // 
+            // label16
+            // 
+            resources.ApplyResources(this.label16, "label16");
+            this.label16.Name = "label16";
+            // 
+            // label17
+            // 
+            resources.ApplyResources(this.label17, "label17");
+            this.label17.Name = "label17";
+            // 
+            // label19
+            // 
+            resources.ApplyResources(this.label19, "label19");
+            this.label19.Name = "label19";
+            // 
+            // label20
+            // 
+            resources.ApplyResources(this.label20, "label20");
+            this.label20.Name = "label20";
+            // 
+            // label21
+            // 
+            resources.ApplyResources(this.label21, "label21");
+            this.label21.Name = "label21";
+            // 
+            // solute
+            // 
+            this.solute.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.solute, "solute");
+            this.solute.Name = "solute";
+            this.solute.UseVisualStyleBackColor = true;
+            this.solute.Click += new System.EventHandler(this.solute_Click);
+            // 
+            // clea_r
+            // 
+            this.clea_r.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.clea_r, "clea_r");
+            this.clea_r.Name = "clea_r";
+            this.clea_r.UseVisualStyleBackColor = true;
+            this.clea_r.Click += new System.EventHandler(this.clea_r_Click);
+            // 
+            // label22
+            // 
+            resources.ApplyResources(this.label22, "label22");
+            this.label22.ForeColor = System.Drawing.Color.Red;
+            this.label22.Name = "label22";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.clea_r);
+            this.Controls.Add(this.solute);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this._r_denominator);
+            this.Controls.Add(this._r_numerator);
+            this.Controls.Add(this.r_denominator);
+            this.Controls.Add(this.r_numerator);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.denominator4);
+            this.Controls.Add(this.numerator4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.denominator3);
+            this.Controls.Add(this.numerator3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.denominator2);
+            this.Controls.Add(this.numerator2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.denominator1);
+            this.Controls.Add(this.numerator1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox numerator1;
+        private System.Windows.Forms.TextBox denominator1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox denominator2;
+        private System.Windows.Forms.TextBox numerator2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox denominator4;
+        private System.Windows.Forms.TextBox numerator4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox denominator3;
+        private System.Windows.Forms.TextBox numerator3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label r_numerator;
+        private System.Windows.Forms.Label r_denominator;
+        private System.Windows.Forms.Label _r_denominator;
+        private System.Windows.Forms.Label _r_numerator;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Button solute;
+        private System.Windows.Forms.Button clea_r;
+        private System.Windows.Forms.Label label22;
+    }
+}
+
