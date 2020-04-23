1a37d0fdbf4a418dc4358246d38c607e03de8dca
Vladislav Taranenko <61828759+TVladOS@users.noreply.github.com>
2020-04-23 17:10:16 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторная 3 CSharp
+{
+   
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clea_r_Click(object sender, EventArgs e)
+        {
+            numerator1.Clear();
+            numerator2.Clear(); 
+            numerator3.Clear(); 
+            numerator4.Clear();
+            denominator1.Clear();
+            denominator2.Clear();
+            denominator3.Clear();
+            denominator4.Clear();
+            label10.Visible = r_numerator.Visible = r_denominator.Visible = label15.Visible = label11.Visible = label12.Visible =
+                    label13.Visible = label14.Visible = label16.Visible = _r_numerator.Visible = _r_denominator.Visible = label22.Visible = false;
+        }
+
+        private void solute_Click(object sender, EventArgs e)
+        {
+
+            Fraction[] fractions = new Fraction[4];
+            for (int i = 0; i < 4; i++)
+            {
+
+                fractions[i] = new Fraction();
+            }
+
+            if (!Int32.TryParse(numerator1.Text, out fractions[0].numerator) ||
+        !Int32.TryParse(denominator1.Text, out fractions[0].denominator) ||
+        !Int32.TryParse(numerator2.Text, out fractions[1].numerator) ||
+        !Int32.TryParse(denominator2.Text, out fractions[1].denominator) ||
+        !Int32.TryParse(numerator3.Text, out fractions[2].numerator) ||
+        !Int32.TryParse(denominator3.Text, out fractions[2].denominator) ||
+        !Int32.TryParse(numerator4.Text, out fractions[3].numerator) ||
+        !Int32.TryParse(denominator4.Text, out fractions[3].denominator))
+                MessageBox.Show("Введите данные корректно"); //Индусская проверка корректности ввода(другой не придумал)
+            for (int i = 0; i < 4; i++)
+            {
+                if (fractions[i].CheckFraction(fractions[i].numerator, fractions[i].denominator) == true)
+                { label22.Visible = true; break; }
+            }
+            if (fractions[3].denominator == 0 || fractions[2].denominator == 0 || fractions[1].denominator == 0 || fractions[0].denominator == 0)
+            {
+                MessageBox.Show("В знаменателе не должно быть 0");
+                clea_r_Click(sender, e);
+                
+                
+            }
+            else
+            {
+                Fraction result = (fractions[0] + fractions[1]) * (fractions[2] - fractions[3]);
+                result = Fraction.Reduction(result);
+                r_numerator.Text = Convert.ToString(result.numerator);
+                r_denominator.Text = Convert.ToString(result.denominator);
+                Fraction turnOv = result;
+                turnOv = Fraction.Turn_over(turnOv);
+                _r_numerator.Text = Convert.ToString(turnOv.numerator);
+                _r_denominator.Text = Convert.ToString(turnOv.denominator);
+                label10.Visible = r_numerator.Visible = r_denominator.Visible = label15.Visible = label11.Visible = label12.Visible =
+                    label13.Visible = label14.Visible = label16.Visible = _r_numerator.Visible = _r_denominator.Visible= true;
+            }
+        }
+
+    }
+
+    }
+
---
+++ Fraction.cs
@@ -0,0 +1,82 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Лабораторная 3 CSharp
+{
+    public class Fraction //Создаем класс Дробь
+    {
+       public Int32 numerator; //Числитель
+       public Int32 denominator; //Знаменатель
+        public Fraction()
+        {
+            numerator = 0;
+            denominator = 0;
+        }
+        public bool CheckFraction(int numerator, int denominator)
+        {
+            if (numerator > denominator)
+                return true; //Если дробь правильная
+            else
+                return false; //Если дробь неправильная
+        }
+       public static Fraction operator +(Fraction first_frac,Fraction second_frac)
+            {
+            if (first_frac.denominator != second_frac.denominator)
+            {
+                second_frac.numerator *= first_frac.denominator;
+                first_frac.denominator *= second_frac.denominator;
+                first_frac.numerator *= second_frac.denominator;
+                second_frac.denominator *= first_frac.denominator;
+                
+                
+            }
+            Fraction end_frac = new Fraction();
+            end_frac.numerator = first_frac.numerator + second_frac.numerator;
+            end_frac.denominator = first_frac.denominator;
+            return end_frac;//new Fraction() { numerator = first_frac.numerator + second_frac.numerator, denominator = first_frac.denominator };
+            }
+        public static Fraction operator -(Fraction first_frac, Fraction second_frac)
+        {
+            if (first_frac.denominator != second_frac.denominator)
+            {
+                first_frac.numerator *= second_frac.denominator;
+                second_frac.numerator *= first_frac.denominator;
+                first_frac.denominator *= second_frac.denominator;
+                second_frac.denominator *= first_frac.denominator;
+                
+            }
+            Fraction end_frac = new Fraction();
+            end_frac.numerator = first_frac.numerator - second_frac.numerator;
+            end_frac.denominator = first_frac.denominator;
+            return end_frac;
+        }
+        public static Fraction operator *(Fraction first_frac, Fraction second_frac)
+        {
+            Fraction end_frac = new Fraction();
+            end_frac.numerator = first_frac.numerator * second_frac.numerator;
+            end_frac.denominator = first_frac.denominator* second_frac.denominator;
+            return end_frac;
+        }
+        public static Fraction Reduction(Fraction obj)
+        {
+            for (int i = 100; i >= 1; i--) //цикл для сокращения дроби
+            {
+                if (obj.numerator % i == 0 && obj.denominator % i == 0)
+                {
+                    obj.numerator /= i;
+                    obj.denominator /= i;
+                }
+            }
+
+
+            return new Fraction() { numerator = obj.numerator, denominator = obj.denominator };
+        }
+        public static Fraction Turn_over(Fraction obj)
+        {
+            return new Fraction() { numerator = obj.denominator, denominator = obj.numerator };
+        }
+    }
+}
